9e60058452ce1e23f21759186129684257b1ce03
derkhadim <dermouhamadou@gmail.com>
2015-06-27 23:51:22 +0000

    Affichage des articles sur la page d index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@posts = Post.all
 	end
 	def show
 	end
@@ -27,7 +28,7 @@ class PostsController < ApplicationController
 	end
 	def destroy
 		@post.destroy
-		redirect_to root_path
+		redirect_to posts_path
 	end
 	private
 		def find_post
